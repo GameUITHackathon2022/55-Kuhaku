f11fc21cfaf73a02f392d594c02307e1a9036600
MingTan <tanlalan123@gmail.com>
2022-11-26 22:51:20 +0700

    Degrade to buildin

--- Assets/Scripts/ChickenControl/AnimalJail.cs
+++ Assets/Scripts/ChickenControl/AnimalJail.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Mono.Cecil;
 using UnityEngine;
 
 public class AnimalJail : MonoBehaviour, ICollisionWithChicken
--- Assets/Scripts/EnemyModule/EnemyScript/EnemyAnimatorController.cs
+++ Assets/Scripts/EnemyModule/EnemyScript/EnemyAnimatorController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.LookDev;
 
 public class EnemyAnimatorController : MonoBehaviour
 {
