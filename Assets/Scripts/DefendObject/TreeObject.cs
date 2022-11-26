885efdb5e929d3b0d12fd2406615ab332278fabd
TaMinhTam <minhtamt1240@gmail.com>
2022-11-26 13:16:45 +0700

    + Add EnemySpawnPoint để sinh enemy ngẫu nhiên

--- Assets/Scripts/DefendObject/TreeObject.cs
+++ Assets/Scripts/DefendObject/TreeObject.cs
@@ -12,9 +12,30 @@ public class TreeObject : DefendBase
         {
             this.level = value;
             this.maxHp = ObjectStatConfigs.Instance.GetHealthBySpecificID(SpecificObjectID, level);
+            this.currentHp = maxHp;
         }
     }
 
-    
+    private void Awake()
+    {
+        LEVEL = level;
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        currentHp = maxHp - currentHp;
+        if (currentHp < 0)
+        { 
+            currentHp = 0;
+            OnDead();
+        }
+    }
+
+    public override void OnDead()
+    {
+        Debug.Log("Dead");
+        Debug.Log("Play Aniamtion and Add Reward To User");
+        Destroy(this.gameObject);
+    }
 
 }
